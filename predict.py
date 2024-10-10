10e8b533a4208c50c3dbf38e716ee8aa9e78d29c
xxxujie <xxxujie@lightfall-wsl.com>
2024-10-11 01:00:55 +0800

    整理代码

--- predict.py
+++ predict.py
@@ -12,11 +12,8 @@ from utils.data_loading import BasicDataset
 from unet import UNet
 from utils.utils import plot_img_and_mask
 
-def predict_img(net,
-                full_img,
-                device,
-                scale_factor=1,
-                out_threshold=0.5):
+
+def predict_img(net, full_img, device, scale_factor=1, out_threshold=0.5):
     net.eval()
     img = torch.from_numpy(BasicDataset.preprocess(None, full_img, scale_factor, is_mask=False))
     img = img.unsqueeze(0)
@@ -24,7 +21,7 @@ def predict_img(net,
 
     with torch.no_grad():
         output = net(img).cpu()
-        output = F.interpolate(output, (full_img.size[1], full_img.size[0]), mode='bilinear')
+        output = F.interpolate(output, (full_img.size[1], full_img.size[0]), mode="bilinear")
         if net.n_classes > 1:
             mask = output.argmax(dim=1)
         else:
@@ -34,27 +31,39 @@ def predict_img(net,
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Predict masks from input images')
-    parser.add_argument('--model', '-m', default='MODEL.pth', metavar='FILE',
-                        help='Specify the file in which the model is stored')
-    parser.add_argument('--input', '-i', metavar='INPUT', nargs='+', help='Filenames of input images', required=True)
-    parser.add_argument('--output', '-o', metavar='OUTPUT', nargs='+', help='Filenames of output images')
-    parser.add_argument('--viz', '-v', action='store_true',
-                        help='Visualize the images as they are processed')
-    parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
-    parser.add_argument('--mask-threshold', '-t', type=float, default=0.5,
-                        help='Minimum probability value to consider a mask pixel white')
-    parser.add_argument('--scale', '-s', type=float, default=0.5,
-                        help='Scale factor for the input images')
-    parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
-    parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
-    
+    parser = argparse.ArgumentParser(description="Predict masks from input images")
+    parser.add_argument(
+        "--model",
+        "-m",
+        default="MODEL.pth",
+        metavar="FILE",
+        help="Specify the file in which the model is stored",
+    )
+    parser.add_argument(
+        "--input", "-i", metavar="INPUT", nargs="+", help="Filenames of input images", required=True
+    )
+    parser.add_argument("--output", "-o", metavar="OUTPUT", nargs="+", help="Filenames of output images")
+    parser.add_argument(
+        "--viz", "-v", action="store_true", help="Visualize the images as they are processed"
+    )
+    parser.add_argument("--no-save", "-n", action="store_true", help="Do not save the output masks")
+    parser.add_argument(
+        "--mask-threshold",
+        "-t",
+        type=float,
+        default=0.5,
+        help="Minimum probability value to consider a mask pixel white",
+    )
+    parser.add_argument("--scale", "-s", type=float, default=0.5, help="Scale factor for the input images")
+    parser.add_argument("--bilinear", action="store_true", default=False, help="Use bilinear upsampling")
+    parser.add_argument("--classes", "-c", type=int, default=2, help="Number of classes")
+
     return parser.parse_args()
 
 
 def get_output_filenames(args):
     def _generate_name(fn):
-        return f'{os.path.splitext(fn)[0]}_OUT.png'
+        return f"{os.path.splitext(fn)[0]}_OUT.png"
 
     return args.output or list(map(_generate_name, args.input))
 
@@ -76,42 +85,40 @@ def mask_to_image(mask: np.ndarray, mask_values):
     return Image.fromarray(out)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
     in_files = args.input
     out_files = get_output_filenames(args)
 
     net = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    logging.info(f'Loading model {args.model}')
-    logging.info(f'Using device {device}')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f"Loading model {args.model}")
+    logging.info(f"Using device {device}")
 
     net.to(device=device)
     state_dict = torch.load(args.model, map_location=device)
-    mask_values = state_dict.pop('mask_values', [0, 1])
+    mask_values = state_dict.pop("mask_values", [0, 1])
     net.load_state_dict(state_dict)
 
-    logging.info('Model loaded!')
+    logging.info("Model loaded!")
 
     for i, filename in enumerate(in_files):
-        logging.info(f'Predicting image {filename} ...')
+        logging.info(f"Predicting image {filename} ...")
         img = Image.open(filename)
 
-        mask = predict_img(net=net,
-                           full_img=img,
-                           scale_factor=args.scale,
-                           out_threshold=args.mask_threshold,
-                           device=device)
+        mask = predict_img(
+            net=net, full_img=img, scale_factor=args.scale, out_threshold=args.mask_threshold, device=device
+        )
 
         if not args.no_save:
             out_filename = out_files[i]
             result = mask_to_image(mask, mask_values)
             result.save(out_filename)
-            logging.info(f'Mask saved to {out_filename}')
+            logging.info(f"Mask saved to {out_filename}")
 
         if args.viz:
-            logging.info(f'Visualizing results for image {filename}, close to continue...')
+            logging.info(f"Visualizing results for image {filename}, close to continue...")
             plot_img_and_mask(img, mask)
--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import argparse
+from gc import enable
 import logging
 import os
 import random
 import sys
 import torch
+import torch.amp
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
@@ -19,24 +21,24 @@ from unet import UNet
 from utils.data_loading import BasicDataset, CarvanaDataset
 from utils.dice_score import dice_loss
 
-dir_img = Path('./data/imgs/')
-dir_mask = Path('./data/masks/')
-dir_checkpoint = Path('./checkpoints/')
+dir_img = Path("./data/imgs/")
+dir_mask = Path("./data/masks/")
+dir_checkpoint = Path("./checkpoints/")
 
 
 def train_model(
-        model,
-        device,
-        epochs: int = 5,
-        batch_size: int = 1,
-        learning_rate: float = 1e-5,
-        val_percent: float = 0.1,
-        save_checkpoint: bool = True,
-        img_scale: float = 0.5,
-        amp: bool = False,
-        weight_decay: float = 1e-8,
-        momentum: float = 0.999,
-        gradient_clipping: float = 1.0,
+    model,
+    device,
+    epochs: int = 5,
+    batch_size: int = 1,
+    learning_rate: float = 1e-5,
+    val_percent: float = 0.1,
+    save_checkpoint: bool = True,
+    img_scale: float = 0.5,
+    amp: bool = False,
+    weight_decay: float = 1e-8,
+    momentum: float = 0.999,
+    gradient_clipping: float = 1.0,
 ):
     # 1. Create dataset
     try:
@@ -55,13 +57,21 @@ def train_model(
     val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
     # (Initialize logging)
-    experiment = wandb.init(project='U-Net', resume='allow', anonymous='must')
+    experiment = wandb.init(project="U-Net", resume="allow", anonymous="must")
     experiment.config.update(
-        dict(epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
-             val_percent=val_percent, save_checkpoint=save_checkpoint, img_scale=img_scale, amp=amp)
+        dict(
+            epochs=epochs,
+            batch_size=batch_size,
+            learning_rate=learning_rate,
+            val_percent=val_percent,
+            save_checkpoint=save_checkpoint,
+            img_scale=img_scale,
+            amp=amp,
+        )
     )
 
-    logging.info(f'''Starting training:
+    logging.info(
+        f"""Starting training:
         Epochs:          {epochs}
         Batch size:      {batch_size}
         Learning rate:   {learning_rate}
@@ -71,12 +81,16 @@ def train_model(
         Device:          {device.type}
         Images scaling:  {img_scale}
         Mixed Precision: {amp}
-    ''')
+    """
+    )
 
     # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
-    optimizer = optim.RMSprop(model.parameters(),
-                              lr=learning_rate, weight_decay=weight_decay, momentum=momentum, foreach=True)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=5)  # goal: maximize Dice score
+    optimizer = optim.RMSprop(
+        model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum, foreach=True
+    )
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, "max", patience=5
+    )  # goal: maximize Dice score
     grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
     criterion = nn.CrossEntropyLoss() if model.n_classes > 1 else nn.BCEWithLogitsLoss()
     global_step = 0
@@ -85,29 +99,32 @@ def train_model(
     for epoch in range(1, epochs + 1):
         model.train()
         epoch_loss = 0
-        with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
+        with tqdm(total=n_train, desc=f"Epoch {epoch}/{epochs}", unit="img") as pbar:
             for batch in train_loader:
-                images, true_masks = batch['image'], batch['mask']
+                images, true_masks = batch["image"], batch["mask"]
 
-                assert images.shape[1] == model.n_channels, \
-                    f'Network has been defined with {model.n_channels} input channels, ' \
-                    f'but loaded images have {images.shape[1]} channels. Please check that ' \
-                    'the images are loaded correctly.'
+                assert images.shape[1] == model.n_channels, (
+                    f"Network has been defined with {model.n_channels} input channels, "
+                    f"but loaded images have {images.shape[1]} channels. Please check that "
+                    "the images are loaded correctly."
+                )
 
                 images = images.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
                 true_masks = true_masks.to(device=device, dtype=torch.long)
 
-                with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+                with torch.autocast(device.type if device.type != "mps" else "cpu", enabled=amp):
                     masks_pred = model(images)
                     if model.n_classes == 1:
                         loss = criterion(masks_pred.squeeze(1), true_masks.float())
-                        loss += dice_loss(F.sigmoid(masks_pred.squeeze(1)), true_masks.float(), multiclass=False)
+                        loss += dice_loss(
+                            F.sigmoid(masks_pred.squeeze(1)), true_masks.float(), multiclass=False
+                        )
                     else:
                         loss = criterion(masks_pred, true_masks)
                         loss += dice_loss(
                             F.softmax(masks_pred, dim=1).float(),
                             F.one_hot(true_masks, model.n_classes).permute(0, 3, 1, 2).float(),
-                            multiclass=True
+                            multiclass=True,
                         )
 
                 optimizer.zero_grad(set_to_none=True)
@@ -120,76 +137,84 @@ def train_model(
                 pbar.update(images.shape[0])
                 global_step += 1
                 epoch_loss += loss.item()
-                experiment.log({
-                    'train loss': loss.item(),
-                    'step': global_step,
-                    'epoch': epoch
-                })
-                pbar.set_postfix(**{'loss (batch)': loss.item()})
+                experiment.log({"train loss": loss.item(), "step": global_step, "epoch": epoch})
+                pbar.set_postfix(**{"loss (batch)": loss.item()})
 
                 # Evaluation round
-                division_step = (n_train // (5 * batch_size))
+                division_step = n_train // (5 * batch_size)
                 if division_step > 0:
                     if global_step % division_step == 0:
                         histograms = {}
                         for tag, value in model.named_parameters():
-                            tag = tag.replace('/', '.')
+                            tag = tag.replace("/", ".")
                             if not (torch.isinf(value) | torch.isnan(value)).any():
-                                histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
+                                histograms["Weights/" + tag] = wandb.Histogram(value.data.cpu())
                             if not (torch.isinf(value.grad) | torch.isnan(value.grad)).any():
-                                histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
+                                histograms["Gradients/" + tag] = wandb.Histogram(value.grad.data.cpu())
 
                         val_score = evaluate(model, val_loader, device, amp)
                         scheduler.step(val_score)
 
-                        logging.info('Validation Dice score: {}'.format(val_score))
+                        logging.info("Validation Dice score: {}".format(val_score))
                         try:
-                            experiment.log({
-                                'learning rate': optimizer.param_groups[0]['lr'],
-                                'validation Dice': val_score,
-                                'images': wandb.Image(images[0].cpu()),
-                                'masks': {
-                                    'true': wandb.Image(true_masks[0].float().cpu()),
-                                    'pred': wandb.Image(masks_pred.argmax(dim=1)[0].float().cpu()),
-                                },
-                                'step': global_step,
-                                'epoch': epoch,
-                                **histograms
-                            })
-                        except:
+                            experiment.log(
+                                {
+                                    "learning rate": optimizer.param_groups[0]["lr"],
+                                    "validation Dice": val_score,
+                                    "images": wandb.Image(images[0].cpu()),
+                                    "masks": {
+                                        "true": wandb.Image(true_masks[0].float().cpu()),
+                                        "pred": wandb.Image(masks_pred.argmax(dim=1)[0].float().cpu()),
+                                    },
+                                    "step": global_step,
+                                    "epoch": epoch,
+                                    **histograms,
+                                }
+                            )
+                        except Exception as e:
+                            logging.error(f"Error logging to W&B: {e}")
                             pass
 
         if save_checkpoint:
             Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
             state_dict = model.state_dict()
-            state_dict['mask_values'] = dataset.mask_values
-            torch.save(state_dict, str(dir_checkpoint / 'checkpoint_epoch{}.pth'.format(epoch)))
-            logging.info(f'Checkpoint {epoch} saved!')
+            state_dict["mask_values"] = dataset.mask_values
+            torch.save(state_dict, str(dir_checkpoint / "checkpoint_epoch{}.pth".format(epoch)))
+            logging.info(f"Checkpoint {epoch} saved!")
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
-    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
-                        help='Learning rate', dest='lr')
-    parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
-    parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
-    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
-                        help='Percent of the data that is used as validation (0-100)')
-    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
-    parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
-    parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
+    parser = argparse.ArgumentParser(description="Train the UNet on images and target masks")
+    parser.add_argument("--epochs", "-e", metavar="E", type=int, default=5, help="Number of epochs")
+    parser.add_argument(
+        "--batch-size", "-b", dest="batch_size", metavar="B", type=int, default=1, help="Batch size"
+    )
+    parser.add_argument(
+        "--learning-rate", "-l", metavar="LR", type=float, default=1e-5, help="Learning rate", dest="lr"
+    )
+    parser.add_argument("--load", "-f", type=str, default=False, help="Load model from a .pth file")
+    parser.add_argument("--scale", "-s", type=float, default=0.5, help="Downscaling factor of the images")
+    parser.add_argument(
+        "--validation",
+        "-v",
+        dest="val",
+        type=float,
+        default=10.0,
+        help="Percent of the data that is used as validation (0-100)",
+    )
+    parser.add_argument("--amp", action="store_true", default=False, help="Use mixed precision")
+    parser.add_argument("--bilinear", action="store_true", default=False, help="Use bilinear upsampling")
+    parser.add_argument("--classes", "-c", type=int, default=2, help="Number of classes")
 
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
 
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    logging.info(f'Using device {device}')
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logging.info(f"Using device {device}")
 
     # Change here to adapt to your data
     # n_channels=3 for RGB images
@@ -197,16 +222,18 @@ if __name__ == '__main__':
     model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
     model = model.to(memory_format=torch.channels_last)
 
-    logging.info(f'Network:\n'
-                 f'\t{model.n_channels} input channels\n'
-                 f'\t{model.n_classes} output channels (classes)\n'
-                 f'\t{"Bilinear" if model.bilinear else "Transposed conv"} upscaling')
+    logging.info(
+        f"Network:\n"
+        f"\t{model.n_channels} input channels\n"
+        f"\t{model.n_classes} output channels (classes)\n"
+        f'\t{"Bilinear" if model.bilinear else "Transposed conv"} upscaling'
+    )
 
     if args.load:
         state_dict = torch.load(args.load, map_location=device)
-        del state_dict['mask_values']
+        del state_dict["mask_values"]
         model.load_state_dict(state_dict)
-        logging.info(f'Model loaded from {args.load}')
+        logging.info(f"Model loaded from {args.load}")
 
     model.to(device=device)
     try:
@@ -218,12 +245,14 @@ if __name__ == '__main__':
             device=device,
             img_scale=args.scale,
             val_percent=args.val / 100,
-            amp=args.amp
+            amp=args.amp,
         )
     except torch.cuda.OutOfMemoryError:
-        logging.error('Detected OutOfMemoryError! '
-                      'Enabling checkpointing to reduce memory usage, but this slows down training. '
-                      'Consider enabling AMP (--amp) for fast and memory efficient training')
+        logging.error(
+            "Detected OutOfMemoryError! "
+            "Enabling checkpointing to reduce memory usage, but this slows down training. "
+            "Consider enabling AMP (--amp) for fast and memory efficient training"
+        )
         torch.cuda.empty_cache()
         model.use_checkpointing()
         train_model(
@@ -234,5 +263,5 @@ if __name__ == '__main__':
             device=device,
             img_scale=args.scale,
             val_percent=args.val / 100,
-            amp=args.amp
+            amp=args.amp,
         )
